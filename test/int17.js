588dee126d87d46484ee2395e938f4fa68dce2c1
neocotic <mercer.alasdair@gmail.com>
2013-02-13 19:09:23 +0000

    fixed many bugs and wrote more tests

--- test/int17.js
+++ test/int17.js
@@ -25,7 +25,7 @@ function equalOnly(test, index, expected, array, message) {
   }
 }
 
-exports.testCreationAndCaching = function(test) {
+exports.testCreate = function(test) {
   var instances = [
       int17.create()
     , int17.create('foo')
@@ -41,3 +41,49 @@ exports.testCreationAndCaching = function(test) {
   test.notEqual(int17.create('foo'), instances[1], 'Cache was not cleared');
   test.done();
 };
+
+// TODO: Test parent filtering
+exports.languages = {
+    testAsync: function(test) {
+      var inst  = int17.create()
+        , langs = ['en', 'en-GB', 'fr-FR'];
+      test.expect(3);
+      inst.init({ path: './test/fixtures/locales2' }, function (err) {
+        test.ifError(err);
+        inst.languages(function (err, languages) {
+          test.ifError(err);
+          test.deepEqual(languages, langs, 'Not all languages were detected');
+          test.done();
+        });
+      });
+    }
+  , testAsyncManual: function(test) {
+      var inst  = int17.create()
+        , langs = ['en-US', 'fr-BE'];
+      test.expect(3);
+      inst.init({ languages: langs, path: './test/fixtures/locales2' }, function (err) {
+        test.ifError(err);
+        inst.languages(function (err, languages) {
+          test.ifError(err);
+          test.deepEqual(languages, langs, 'Configured languages should have been used');
+          test.done();
+        });
+      });
+    }
+  , testSync: function(test) {
+      var inst  = int17.create()
+        , langs = ['en', 'en-GB', 'fr-FR'];
+      inst.initSync({ path: './test/fixtures/locales2' });
+      test.deepEqual(inst.languagesSync(), langs, 'Not all languages were detected');
+      test.done();
+    }
+  , testSyncManual: function(test) {
+      var inst  = int17.create()
+        , langs = ['en-US', 'fr-BE'];
+      inst.initSync({ languages: langs, path: './test/fixtures/locales2' });
+      test.deepEqual(inst.languagesSync(), langs, 'Configured languages should have been used');
+      test.done();
+    }
+};
+
+// TODO: Complete test cases
