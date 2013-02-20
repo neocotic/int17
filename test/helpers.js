3cfe2d08bdcf3993212cc750456201e98c990f24
neocotic <mercer.alasdair@gmail.com>
2013-02-20 14:47:45 +0000

    made test helper function arguments clearer

--- test/helpers.js
+++ test/helpers.js
@@ -7,26 +7,26 @@
 
   // Test that `actual` contains all the same properties of `expected` - with equal values, while
   // it may also contain more.
-  Helpers.prototype.contains = function(ctx, actual, expected, message, strict) {
+  Helpers.prototype.contains = function(test, actual, expected, message, strict) {
     message = message ? message + ': ' : '';
     var prop
       , equalMethod = strict ? 'strictEqual' : 'equal';
     for (prop in expected) {
       if (!expected.hasOwnProperty(prop)) continue;
-      ctx[equalMethod](actual[prop], expected[prop], message + 'property[' + prop +
+      test[equalMethod](actual[prop], expected[prop], message + 'property[' + prop +
         '] was not as expected');
     }
   };
 
   // Test that `actual` contains all the same properties of `expected` - with *strictly* equal
   // values, while it may also contain more.
-  Helpers.prototype.strictContains = function(ctx, actual, expected, message, strict) {
-    this.contains(ctx, actual, expected, message, true);
+  Helpers.prototype.strictContains = function(test, actual, expected, message, strict) {
+    this.contains(test, actual, expected, message, true);
   };
 
   // Test that the item at `index` within `array` is only equal to those within the `expected`
   // indices.
-  Helpers.prototype.equalOnly = function(ctx, index, expected, array, message, strict) {
+  Helpers.prototype.equalOnly = function(test, index, expected, array, message, strict) {
     message = message ? message + ': ' : '';
     var i, isExpected, j
       , equalMethod    = strict ? 'strictEqual' : 'equal'
@@ -42,10 +42,10 @@
         }
       }
       if (isExpected) {
-        ctx[equalMethod](array[i], array[index], message + 'item[' + i + '] should equal item[' +
+        test[equalMethod](array[i], array[index], message + 'item[' + i + '] should equal item[' +
           index + ']');
       } else {
-        ctx[notEqualMethod](array[i], array[index], message + 'item[' + i +
+        test[notEqualMethod](array[i], array[index], message + 'item[' + i +
           '] should not equal item[' + index + ']');
       }
     }
@@ -53,8 +53,8 @@
 
   // Test that the item at `index` within `array` is only *strictly* equal to those within the
   // `expected` indices.
-  Helpers.prototype.strictEqualOnly = function(ctx, index, expected, array, message) {
-    this.equalOnly(ctx, index, expected, array, message, true);
+  Helpers.prototype.strictEqualOnly = function(test, index, expected, array, message) {
+    this.equalOnly(test, index, expected, array, message, true);
   };
 
   // Expose an instance of `Helpers` for node.js or the browser.
